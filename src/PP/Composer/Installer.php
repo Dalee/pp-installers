d76ed51aa6be0bdf93dc829a4eb87367d00dcc86
Alexej Yaroshevich <alex@qfox.ru>
2015-02-08 22:48:06 +0400

    initial

---
+++ src/PP/Composer/Installer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PP\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class Installer extends LibraryInstaller {
+    /**
+     * {@inheritDoc}
+     */
+    public function getPackageBasePath(PackageInterface $package) {
+        $name = $package->getPrettyName();
+        if ($name === 'dalee/libpp5') {
+            return 'libpp';
+        }
+
+        $prefix = substr($name, 0, 3);
+        if ('pp/' !== $prefix) {
+            throw new \InvalidArgumentException(
+                'Unable to install plugin. PP plugins '
+                .'should always start their package name with '
+                .'"pp/"'
+            );
+        }
+
+        return 'local/plugins/' . substr($name, 3);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType) {
+        return 'pp-plugin' === $packageType;
+    }
+}
---
+++ src/PP/Composer/InstallerPlugin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PP\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class InstallerPlugin implements PluginInterface {
+    public function activate(Composer $composer, IOInterface $io) {
+        $installer = new Installer($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
